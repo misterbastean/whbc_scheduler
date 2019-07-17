1f54fdf72cb78ff087b00dd155675c86d6fb5623
misterbastean <misterbastean@gmail.com>
2019-07-17 13:52:43 -0400

    Add worker/participant update routes barebones

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,9 @@
 const express       = require('express'),
       router        = express.Router();
 
+// =============================
+// EVENTS
+// =============================
 router.get('/', (req, res) => {
   res.send("Show events index")
 });
@@ -29,20 +32,44 @@ router.delete('/:id', (req, res) => {
   res.send(`Delete event with ID of ${req.params.id}, then redirect to /events`)
 });
 
-router.get('/:id/workers', (req, res) => {
-  res.send(`Show form to add workers to event with ID of ${req.params.id}`)
+// =============================
+// EVENT WORKERS
+// =============================
+
+router.get('/:id/workers/new', (req, res) => {
+  res.send(`Show form to add worker(s) to event with ID of ${req.params.id}`)
 });
 
 router.post('/:id/workers', (req, res) => {
-  res.send(`Add submitted workers to event with ID of ${req.params.id}, the redirect to that events information page`)
+  res.send(`Add submitted worker(s) to event with ID of ${req.params.id}, the redirect to that events information page`)
 });
 
-router.get('/:id/participants', (req, res) => {
-  res.send(`Show form to add participants to event with ID of ${req.params.id}`)
+router.get('/:id/workers/edit', (req, res) => {
+  res.send(`Show form to edit workers for event with ID of ${req.params.id}`)
+});
+
+router.put('/:id/workers', (req, res) => {
+  res.send(`Update workers for event with ID of ${req.params.id}`)
+});
+
+// =============================
+// EVENT PARTICIPANTS
+// =============================
+
+router.get('/:id/participants/new', (req, res) => {
+  res.send(`Show form to add participant(s) to event with ID of ${req.params.id}`)
 });
 
 router.post('/:id/participants', (req, res) => {
-  res.send(`Add submitted participants to event with ID of ${req.params.id}, the redirect to that events information page`)
+  res.send(`Edit participant(s) for event with ID of ${req.params.id}, the redirect to that events information page`)
+});
+
+router.get('/:id/participants/edit', (req, res) => {
+  res.send(`Show form to edit participants for event with ID of ${req.params.id}`)
+});
+
+router.put('/:id/participants', (req, res) => {
+  res.send(`Update participants for event with ID of ${req.params.id}`)
 });
 
 
