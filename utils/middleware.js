bd672d7049894206f64d80a843b52656397b938d
misterbastean <misterbastean@gmail.com>
2019-07-26 12:02:38 -0400

    Add logic to check event ownership

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
 let middlewareObj = {};
+const Event       = require('../models/event')
 
 // Check if user is logged in; redirect if not
 middlewareObj.isLoggedIn = (req, res, next) => {
@@ -34,5 +35,41 @@ middlewareObj.isAdmin = (req, res, next) => {
     res.redirect("/login")
   }
 }
+
+// Check if user owns an event
+middlewareObj.checkEventOwnership = (req, res, next) => {
+  console.log("check 0");
+  // Check if user is logged in
+  if (req.isAuthenticated()) {
+    console.log("check 1");
+    // Find the event from the DB
+    Event.findById(req.params.id, (err, foundEvent) => {
+      if (err || !foundEvent) {
+        console.log("Comment not found");
+        res.redirect(back)
+      } else {
+        // Does the user own the event?
+        console.log("check 2");
+        if (foundEvent.owner.id.equals(req.user._id)) {
+          console.log("check 3");
+          next();
+        } else {
+          console.log("You don't have permission to do that");
+          res.redirect('back')
+        }
+      }
+    })
+  } else {
+    console.log("You need to be logged in to do that");
+    res.redirect("back");
+  }
+
+}
+
+// Check if user owns a user profile
+
+// Check if user owns a worker
+
+// Check if user owns a participant
 
 module.exports = middlewareObj;
